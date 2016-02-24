4c656b860e0c2b56df65632e0c4872578732bbf2
Colin Herzog <colin.herzog@rhino-it.ch>
2016-02-24 15:43:36 +0100

    Added GUI-Package

---
+++ src/main/java/ch/gibmit/m226/todo/gui/guiMain.java
@@ -0,0 +1,37 @@
+package ch.gibmit.m226.todo.gui;
+
+import javax.swing.*;
+
+/**
+ * Created by colin on 24.02.16.
+ */
+public class guiMain extends JFrame {
+
+    JTabbedPane tabbedPane;
+
+    public guiMain() {
+        tabbedPane = new JTabbedPane();
+        this.add(tabbedPane);
+        JComponent panel1 = makeTextPanel("Panel #1");
+        tabbedPane.addTab("Tab 1", icon, panel1,
+                "Does nothing");
+        tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+
+        JComponent panel2 = makeTextPanel("Panel #2");
+        tabbedPane.addTab("Tab 2", icon, panel2,
+                "Does twice as much nothing");
+        tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
+
+        JComponent panel3 = makeTextPanel("Panel #3");
+        tabbedPane.addTab("Tab 3", icon, panel3,
+                "Still does nothing");
+        tabbedPane.setMnemonicAt(2, KeyEvent.VK_3);
+
+        JComponent panel4 = makeTextPanel(
+                "Panel #4 (has a preferred size of 410 x 50).");
+        panel4.setPreferredSize(new Dimension(410, 50));
+        tabbedPane.addTab("Tab 4", icon, panel4,
+                "Does nothing at all");
+        tabbedPane.setMnemonicAt(3, KeyEvent.VK_4);
+    }
+}
