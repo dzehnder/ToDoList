e3a9cb76f5f21626d98e7eedd1c03afffa42dc0b
Damian Zehnder <damian.zehnder@gibmit.ch>
2016-04-15 14:53:14 +0200

    repeater setting ui working

--- src/main/java/ch/gibmit/m226/todo/gui/guiToDoImpl/GuiToDoRepeatImpl.java
+++ src/main/java/ch/gibmit/m226/todo/gui/guiToDoImpl/GuiToDoRepeatImpl.java
@@ -1,9 +1,10 @@
 package ch.gibmit.m226.todo.gui.guiToDoImpl;
 
+import org.jdesktop.swingx.JXDatePicker;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 /**
  * @author Damian Zehnder
@@ -14,12 +15,14 @@ import java.awt.event.ActionListener;
 public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
 
     private static final String[] REPEATRATES = {"Never", "Daily", "Weekly", "Monthly", "Yearly"};
-    private static final String[] WEEKDAYS = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    private static final String[] WEEKDAYS = {"Monday   ", "Tuesday  ", "Wednesday", "Thursday ", "Friday   ", "Saturday ", "Sunday   "};
 
     private JDialog dlgRepeater;
     private JPanel pnlRepeater;
     private JPanel pnlRepeaterCenter;
+    private JPanel pnlRepeaterCenterComp;
     private JPanel pnlRepeaterBottom;
+    private JPanel pnlRepeaterUntilDate;
     private JLabel lblRepetition;
     private JSpinner spnrTime;
     private JLabel lblRate;
@@ -28,6 +31,7 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
     private JCheckBox[] chbxWeekdays = new JCheckBox[WEEKDAYS.length];
     private JComboBox<String> cmbxRepeatRate;
     private JComboBox<String> cmbxEndDate;
+    private JXDatePicker xdpEndDate;
 
 
     /**
@@ -36,11 +40,17 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
     public GuiToDoRepeatImpl() {
 
         dlgRepeater = new JDialog();
+
         btnOk = new JButton("Ok");
         dlgRepeater.getRootPane().setDefaultButton(btnOk);
         btnOk.isDefaultButton();
+        btnOk.addActionListener(this);
+
         btnCancel = new JButton("Cancel");
+        btnCancel.addActionListener(this);
+
         cmbxEndDate = new JComboBox<>(new String[]{"No end date", "End by date: "});
+        cmbxEndDate.addActionListener(this);
 
         for (int i = 0; i < chbxWeekdays.length; i++) {
             chbxWeekdays[i] = new JCheckBox(WEEKDAYS[i]);
@@ -51,8 +61,8 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
         updateContentToSelection();
 
         dlgRepeater.setTitle("Repeater options");
-        //setResizable(false);
-        dlgRepeater.setSize(new Dimension(300, 200));
+        setResizable(false);
+        dlgRepeater.setSize(new Dimension(300, 160));
         dlgRepeater.setLocationRelativeTo(SwingUtilities.getWindowAncestor(this));
         dlgRepeater.setModal(true);
         dlgRepeater.setVisible(true);
@@ -63,12 +73,16 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
      */
     private void setUpPanels() {
         pnlRepeater = new JPanel(new BorderLayout());
-        pnlRepeaterCenter = new JPanel();
+        pnlRepeaterCenter = new JPanel(new BorderLayout());
         pnlRepeaterBottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        pnlRepeaterCenterComp = new JPanel();
+        pnlRepeaterUntilDate = new JPanel(new BorderLayout());
 
         dlgRepeater.add(pnlRepeater);
         pnlRepeater.add(pnlRepeaterBottom, BorderLayout.PAGE_END);
-        pnlRepeater.add(pnlRepeaterCenter);
+        pnlRepeater.add(pnlRepeaterCenter, BorderLayout.CENTER);
+        pnlRepeaterCenter.add(pnlRepeaterCenterComp, BorderLayout.CENTER);
+        pnlRepeaterCenter.add(pnlRepeaterUntilDate, BorderLayout.PAGE_END);
     }
 
     /**
@@ -78,7 +92,9 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
         cmbxRepeatRate = new JComboBox<>();
         lblRepetition = new JLabel("Every: ");
         spnrTime = new JSpinner();
+        spnrTime.setPreferredSize(new Dimension(55, 20));
         lblRate = new JLabel();
+        xdpEndDate = new JXDatePicker();
 
         placeComponents();
 
@@ -88,6 +104,7 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
 
         cmbxRepeatRate.addActionListener(this);
         pnlRepeater.add(cmbxRepeatRate, BorderLayout.PAGE_START);
+
         pnlRepeaterBottom.add(btnCancel);
         pnlRepeaterBottom.add(btnOk);
     }
@@ -96,29 +113,63 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
      * adds the necessary components for the day, month and year repeat rate
      */
     private void placeComponents() {
-        pnlRepeaterCenter.setLayout(new FlowLayout());
-        pnlRepeaterCenter.add(lblRepetition);
-        pnlRepeaterCenter.add(spnrTime);
-        pnlRepeaterCenter.add(lblRate);
-        //pnlRepeaterCenter.add(cmbxEndDate, BorderLayout.PAGE_END);
+        pnlRepeaterCenterComp.setLayout(new FlowLayout());
+        pnlRepeaterCenterComp.add(lblRepetition);
+        pnlRepeaterCenterComp.add(spnrTime);
+        pnlRepeaterCenterComp.add(lblRate);
+        pnlRepeaterUntilDate.add(cmbxEndDate, BorderLayout.LINE_START);
 
-        pnlRepeaterCenter.repaint();
+        updateRepeatDateToSelection();
+        pnlRepeaterUntilDate.repaint();
+        pnlRepeaterCenterComp.repaint();
     }
 
     /**
      * removes the necessary components for the day, month and year repeat rate
      */
     private void removeComponents() {
-        pnlRepeaterCenter.removeAll();
-        pnlRepeaterCenter.repaint();
+        pnlRepeaterCenterComp.removeAll();
+        pnlRepeaterCenterComp.repaint();
+        pnlRepeaterUntilDate.removeAll();
+        pnlRepeaterUntilDate.repaint();
+
+        dlgRepeater.setMinimumSize(new Dimension(300, 160));
+        dlgRepeater.setSize(new Dimension(300, 160));
     }
 
+    /**
+     * places the weekday-checkboxes in the center of the dialog
+     */
     private void placeWeekComponents() {
-        pnlRepeaterCenter.setLayout(new BoxLayout(pnlRepeaterCenter, BoxLayout.PAGE_AXIS));
-        for (JCheckBox chbxWeekday : chbxWeekdays) {
-            pnlRepeaterCenter.add(chbxWeekday, Component.CENTER_ALIGNMENT);
+        pnlRepeaterCenterComp.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        JPanel fillLeft = new JPanel();
+        fillLeft.setPreferredSize(new Dimension(dlgRepeater.getWidth()/3, 1));
+        c.gridx = 0;
+        c.gridy = 0;
+        pnlRepeaterCenterComp.add(fillLeft, c);
+
+        JPanel fillRight = new JPanel();
+        fillRight.setPreferredSize(new Dimension(dlgRepeater.getWidth()/3, 1));
+        c.gridx = 2;
+        c.gridy = 0;
+        pnlRepeaterCenterComp.add(fillRight, c);
+
+        for (int i = 0; i < chbxWeekdays.length; i++) {
+            JCheckBox chbxWeekday = chbxWeekdays[i];
+            c.gridx = 1;
+            c.gridy = i;
+            c.anchor = GridBagConstraints.LINE_START;
+            pnlRepeaterCenterComp.add(chbxWeekday, c);
         }
-        pnlRepeaterCenter.revalidate();
+        pnlRepeaterUntilDate.add(cmbxEndDate, BorderLayout.LINE_START);
+        updateRepeatDateToSelection();
+        dlgRepeater.setMinimumSize(new Dimension(300, 300));
+
+        pnlRepeaterUntilDate.repaint();
+        pnlRepeaterCenterComp.revalidate();
+
     }
 
 
@@ -152,6 +203,21 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
     }
 
     /**
+     * adds or removes the until date-picker according to the selected option
+     */
+    private void updateRepeatDateToSelection() {
+        switch (cmbxEndDate.getSelectedIndex()) {
+            case 0:
+                pnlRepeaterUntilDate.remove(xdpEndDate);
+                pnlRepeaterUntilDate.revalidate();
+                break;
+            case 1:
+                pnlRepeaterUntilDate.add(xdpEndDate, BorderLayout.CENTER);
+                pnlRepeaterUntilDate.revalidate();
+        }
+    }
+
+    /**
      * Handles the actions performed in the dialog
      *
      * @param e the fired action event
@@ -160,6 +226,15 @@ public class GuiToDoRepeatImpl extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.cmbxRepeatRate) {
             updateContentToSelection();
+        }
+        if (e.getSource() == this.cmbxEndDate) {
+            updateRepeatDateToSelection();
+        }
+        if (e.getSource() == this.btnCancel) {
+            dlgRepeater.dispose();
+        }
+        if (e.getSource() == this.btnOk) {
+            // do stuff
         }
 
     }
